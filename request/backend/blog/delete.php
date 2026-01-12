98703b092c8573bdb5f178f6a0ad74f970738ae9
SMTECHSOFT <smtechsoftltd@gmail.com>
2026-01-12 10:58:09 +0600

    Update delete.php

--- request/backend/blog/delete.php
+++ request/backend/blog/delete.php
@@ -6,8 +6,20 @@ $db = new DbQuery();
 $id = $_POST['id'] ?? null;
 
 if ($id) {
-    $db->delete('blogs', $id);
-    header('Location: /admin/blogs?success=Blog deleted successfully');
+    $blog = $db->find('blogs', $id);
+    if ($blog['rowCount'] > 0) {
+        $thumbnail = $blog['row']->thumbnail;
+        if ($thumbnail) {
+            $filePath = dirname(__DIR__, 3) . $thumbnail;
+            if (file_exists($filePath)) {
+                unlink($filePath);
+            }
+        }
+        $db->delete('blogs', $id);
+        header('Location: /admin/blogs?success=Blog deleted successfully');
+    } else {
+        header('Location: /admin/blogs?error=Blog not found');
+    }
 } else {
     header('Location: /admin/blogs?error=Invalid Blog ID');
 }

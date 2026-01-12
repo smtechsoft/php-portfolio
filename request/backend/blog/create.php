043174f2c78ff8a4c81c2817c78722182c38e5bb
SMTECHSOFT <smtechsoftltd@gmail.com>
2026-01-12 10:57:49 +0600

    added thumb upload feature in blog

--- request/backend/blog/create.php
+++ request/backend/blog/create.php
@@ -3,25 +3,39 @@
 use App\Services\DbQuery;
 use App\Services\SlugService;
 
-$slug = new SlugService();
-$db =  new DbQuery();
-$title = $_POST['title'] ?? '';
-$thumbnail = $_FILES['thumbnail'] ?? null;
-$newSlug = '';
-$thumName = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = $_POST['title'];
+    $slugService = new SlugService();
+    $slug = $slugService->generate('blogs', $title);
+    $thumbnailPath = null;
 
-if (!empty($title)) {
-    $newSlug = $slug->generate('blogs', $title);
+    if (isset($_FILES['thumbnail']) && $_FILES['thumbnail']['error'] === UPLOAD_ERR_OK) {
+        $uploadDir = dirname(__DIR__, 3) . '/uploads/img/blog/';
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        $fileInfo = pathinfo($_FILES['thumbnail']['name']);
+        $extension = $fileInfo['extension'];
+        $uniqueName = time() . '_' . uniqid() . '.' . $extension;
+        $targetFile = $uploadDir . $uniqueName;
+
+        if (move_uploaded_file($_FILES['thumbnail']['tmp_name'], $targetFile)) {
+            $thumbnailPath = '/uploads/img/blog/' . $uniqueName;
+        }
+    }
+
+    $db = new DbQuery();
+    $result = $db->insert('blogs', [
+        'title' => $title,
+        'slug' => $slug,
+        'thumbnail' => $thumbnailPath
+    ]);
+
+    if ($result === 'data inserted') {
+        header('Location: /admin/blogs');
+        exit;
+    } else {
+        echo "Error creating blog";
+    }
 }
-
-if (isset($_FILES['thumbnail'])) {
-    print_r($_FILES['thumbnail']);
-    $thumName = '/uploads/img/blogs/';
-}
-
-$db->insert('blogs', [
-    'title' => $title,
-    'slug' => $newSlug,
-    'thumbnail' => $thumName
-]);
-header('Location:/admin/blogs');
--- request/backend/blog/update.php
+++ request/backend/blog/update.php
@@ -9,12 +9,37 @@ $thumbnail = $_FILES['thumbnail'] ?? null;
 
 $getItem = $db->findBySlug('blogs', $slug);
 if ($getItem['rowCount'] > 0) {
-    $db->update('blogs', $getItem['row']->id, [
+    $updateData = [
         'title' => $title,
-    ]);
+    ];
+
+    if (isset($_FILES['thumbnail']) && $_FILES['thumbnail']['error'] === UPLOAD_ERR_OK) {
+        $uploadDir = dirname(__DIR__, 3) . '/uploads/img/blog/';
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        // Delete old thumbnail
+        $oldThumbnail = $getItem['row']->thumbnail;
+        if ($oldThumbnail) {
+            $oldFilePath = dirname(__DIR__, 3) . $oldThumbnail;
+            if (file_exists($oldFilePath)) {
+                unlink($oldFilePath);
+            }
+        }
+
+        $fileInfo = pathinfo($_FILES['thumbnail']['name']);
+        $extension = $fileInfo['extension'];
+        $uniqueName = time() . '_' . uniqid() . '.' . $extension;
+        $targetFile = $uploadDir . $uniqueName;
+
+        if (move_uploaded_file($_FILES['thumbnail']['tmp_name'], $targetFile)) {
+            $updateData['thumbnail'] = '/uploads/img/blog/' . $uniqueName;
+        }
+    }
+
+    $db->update('blogs', $getItem['row']->id, $updateData);
     header('Location: /admin/blogs?success=Blog updated successfully');
 } else {
     die('Blog not found');
 }
-// echo $getItem['row']->id;
-// print_r($getItem);

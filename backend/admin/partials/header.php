b9b9911331c7f13538d7cf11b618fdc3c52d6473
SMTECHSOFT <smtechsoftltd@gmail.com>
2026-01-12 11:08:07 +0600

    Update header.php

--- backend/admin/partials/header.php
+++ backend/admin/partials/header.php
@@ -12,6 +12,9 @@ if (!isset($_SESSION['user_id'])) {
         <a href="/admin/blogs">Blogs</a>
     </li>
     <li>
+        <a href="/admin/pricing">Pricing</a>
+    </li>
+    <li>
         <form action="/request/backend/auth/logout" method="post">
             <button type="submit">Logout</button>
         </form>
